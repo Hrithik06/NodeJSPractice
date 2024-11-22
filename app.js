cb99bdbbcdbb1a4946a8e7e3c6d4b60201906c54
Hrithik06 <el99el03@gmail.com>
2024-11-22 10:30:18 +0530

    understanding require

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 console.log("HI");
+require("./xyz");
 
 const obj = require("./sum");
 console.log(obj.x);
@@ -8,3 +9,17 @@ const { x, calSum } = require("./sum");
 console.log(x);
 calSum(6, 5);
 z = 10;
+
+// console.log(require("fs").readFileSync.toString());
+
+function Module(id = "", parent) {
+  this.id = id;
+  this.path = path.dirname(id);
+  setOwnProperty(this, "exports", {});
+  this[kModuleParent] = parent;
+  updateChildren(parent, this, false);
+  this.filename = null;
+  this.loaded = false;
+  this.children = [];
+}
+console.log(Module);
---
+++ xyz.js
@@ -0,0 +1,17 @@
+console.log(require("module").wrapper);
+console.log("*".repeat(20));
+
+console.log("__dirname:\n", __dirname);
+console.log("*".repeat(20));
+
+console.log("__filename: \n", __filename);
+console.log("*".repeat(20));
+
+console.log("exports: \n", exports);
+console.log("*".repeat(20));
+
+console.log("require: \n", require);
+console.log("*".repeat(20));
+
+console.log("module: \n", module);
+console.log("*".repeat(20));
