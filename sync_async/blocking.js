6bce9828df09d320887a89132480f76fd779d9a7
Hrithik06 <el99el03@gmail.com>
2024-11-24 16:30:46 +0530

    blocking code

--- sync_async/blocking.js
+++ sync_async/blocking.js
@@ -7,9 +7,12 @@ var b = 23423;
 //Sync Funs block Main Thread
 crypto.pbkdf2Sync("password", "salt", 5000000, 20, "sha512");
 console.log("First Key Generated Sync");
+
+//Will be called when call stack of Main thread is empty
 setTimeout(function () {
   console.log("CALL ME RIGHT NOW!!!!!!!");
 }, 0);
+
 //Async Fun
 crypto.pbkdf2("password", "salt", 5000000, 20, "sha512", (err, key) => {
   console.log("Second Key Generated ASync");
