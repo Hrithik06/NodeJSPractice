28e198d5c38c38b4c986d9ce425946286c260ca6
Hrithik06 <el99el03@gmail.com>
2025-02-01 13:41:53 +0530

    db

--- mongodb/database.js
+++ mongodb/database.js
@@ -1,74 +1,74 @@
-const { MongoClient } = require("mongodb");
+const { MongoClient } = require('mongodb')
 
 const url =
-  "mongodb+srv://writhikdev:UlylqYIMuyGrt1nf@helloworldcluster.xryir.mongodb.net/?retryWrites=true&w=majority&appName=HelloWorldCluster";
+    'mongodb+srv://writhikdev:<db_password>@helloworldcluster.xryir.mongodb.net/?retryWrites=true&w=majority&appName=HelloWorldCluster'
 
-const client = new MongoClient(url);
+const client = new MongoClient(url)
 
-const dbName = "HelloWorldDB";
+const dbName = 'HelloWorldDB'
 const main = async () => {
-  await client.connect();
-  console.log("Connected successfully to server");
-  const db = client.db(dbName);
-  const collection = db.collection("user");
+    await client.connect()
+    console.log('Connected successfully to server')
+    const db = client.db(dbName)
+    const collection = db.collection('user')
 
-  //   //insert One Document
-  //   const datum = {
-  //     firstname: "Abhi",
-  //     lastname: "Ab",
-  //     city: "Bangalore",
-  //     phone: "9876543210",
-  //   };
-  //   const insertDatum = await collection.insertOne(datum);
-  //   console.log("Inserted document =>", insertDatum);
+    //   //insert One Document
+    //   const datum = {
+    //     firstname: "Abhi",
+    //     lastname: "Ab",
+    //     city: "Bangalore",
+    //     phone: "9876543210",
+    //   };
+    //   const insertDatum = await collection.insertOne(datum);
+    //   console.log("Inserted document =>", insertDatum);
 
-  //   //insert Multiple Document
-  //   const data = {
-  //     firstname: "Abhi",
-  //     lastname: "Ab",
-  //     city: "Bangalore",
-  //     phone: "9876543210",
-  //   };
-  //   const insertData = await collection.insertMany([
-  //     data,
-  //     {
-  //       firstname: "Gagan",
-  //       lastname: "Ab",
-  //       city: "Bangalore",
-  //       phone: "9876543210",
-  //     },
-  //   ]);
-  //   console.log("Inserted documents =>", insertData);
+    //   //insert Multiple Document
+    //   const data = {
+    //     firstname: "Abhi",
+    //     lastname: "Ab",
+    //     city: "Bangalore",
+    //     phone: "9876543210",
+    //   };
+    //   const insertData = await collection.insertMany([
+    //     data,
+    //     {
+    //       firstname: "Gagan",
+    //       lastname: "Ab",
+    //       city: "Bangalore",
+    //       phone: "9876543210",
+    //     },
+    //   ]);
+    //   console.log("Inserted documents =>", insertData);
 
-  //Filter Data
-  const filterData = await collection.find({ firstname: "Abhi" }).toArray();
-  console.log("filterData: ", filterData);
-  const countFilterData = await collection.countDocuments({
-    firstname: "Abhi",
-  });
-  console.log("countFilterData: ", countFilterData);
+    //Filter Data
+    const filterData = await collection.find({ firstname: 'Abhi' }).toArray()
+    console.log('filterData: ', filterData)
+    const countFilterData = await collection.countDocuments({
+        firstname: 'Abhi',
+    })
+    console.log('countFilterData: ', countFilterData)
 
-  //   //Delete Many
-  //   const deleteMany = await collection.deleteMany({ firstname: "Abhi" });
-  //   console.log("deleteMany: ", deleteMany);
+    //   //Delete Many
+    //   const deleteMany = await collection.deleteMany({ firstname: "Abhi" });
+    //   console.log("deleteMany: ", deleteMany);
 
-  //   //Delete One
-  //   const delOne = await collection.deleteOne({ firstname: "Abhi" });
-  //   console.log("delOne: ", delOne);
-  // Update One
+    //   //Delete One
+    //   const delOne = await collection.deleteOne({ firstname: "Abhi" });
+    //   console.log("delOne: ", delOne);
+    // Update One
 
-  const updateResult = await collection.updateOne(
-    { firstname: "Abhi" },
-    { $set: { lastname: "Lavde" } }
-  );
-  console.log("Updated documents =>", updateResult);
-  //Find All Docs in a collection
-  const userData = await collection.find({}).toArray();
-  console.log("userData => ", userData);
-  return "done.";
-};
+    const updateResult = await collection.updateOne(
+        { firstname: 'Abhi' },
+        { $set: { lastname: 'Lavde' } }
+    )
+    console.log('Updated documents =>', updateResult)
+    //Find All Docs in a collection
+    const userData = await collection.find({}).toArray()
+    console.log('userData => ', userData)
+    return 'done.'
+}
 
 main()
-  .then(console.log)
-  .catch(console.error)
-  .finally(() => client.close());
+    .then(console.log)
+    .catch(console.error)
+    .finally(() => client.close())
